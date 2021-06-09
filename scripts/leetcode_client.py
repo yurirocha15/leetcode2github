b83fee7ec695ab6c2a6a8b947e7c176bce8a3332
Yuri Rocha <yurirocha15@gmail.com>
2021-06-09 16:14:45 +0900

    removed useless query

--- scripts/leetcode_client.py
+++ scripts/leetcode_client.py
@@ -164,7 +164,43 @@ class LeetcodeClient:
             {
                 "operationName": "questionData",
                 "variables": {"titleSlug": question_name},
-                "query": "query questionData($titleSlug: String) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    boundTopicId\n    title\n    titleSlug\n    content\n    translatedTitle\n    translatedContent\n    isPaidOnly\n    difficulty\n    likes\n    dislikes\n    isLiked\n    similarQuestions\n    exampleTestcases\n    contributors {\n      username\n      profileUrl\n      avatarUrl\n      __typename\n    }\n    topicTags {\n      name\n      slug\n      translatedName\n      __typename\n    }\n    companyTagStats\n    codeSnippets {\n      lang\n      langSlug\n      code\n      __typename\n    }\n    stats\n    hints\n    solution {\n      id\n      canSeeDetail\n      paidOnly\n      hasVideoSolution\n      paidOnlyVideo\n      __typename\n    }\n    status\n    sampleTestCase\n    metaData\n    judgerAvailable\n    judgeType\n    mysqlSchemas\n    enableRunCode\n    enableTestMode\n    enableDebugger\n    envInfo\n    libraryUrl\n    adminUrl\n    __typename\n  }\n}\n",
+                "query": "query questionData($titleSlug: String) {\n  question(titleSlug: $titleSlug) {\
+                    \n    questionId\
+                    \n    questionFrontendId\
+                    \n    title\
+                    \n    titleSlug\
+                    \n    content\
+                    \n    isPaidOnly\
+                    \n    difficulty\
+                    \n    likes\
+                    \n    dislikes\
+                    \n    exampleTestcases\
+                    \n    topicTags {\
+                    \n      name\
+                    \n      slug\
+                    \n      translatedName\
+                    \n      __typename\
+                    \n    }\
+                    \n    codeSnippets {\
+                    \n      lang\
+                    \n      langSlug\
+                    \n      code\
+                    \n      __typename\
+                    \n    }\
+                    \n    stats\
+                    \n    hints\
+                    \n    solution {\
+                    \n      id\
+                    \n      canSeeDetail\
+                    \n      paidOnly\
+                    \n      hasVideoSolution\
+                    \n      paidOnlyVideo\
+                    \n      __typename\
+                    \n    }\
+                    \n    status\
+                    \n    sampleTestCase\
+                    \n    metaData\
+                    \n    __typename\n  }\n}\n",
             }
         )
 
