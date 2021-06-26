30f61b433ad08ac3db46e02a3a730b470f6bad4c
Yuri Rocha <yurirocha15@gmail.com>
2021-06-26 19:12:24 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,49 @@
+import os
+import re
+from glob import glob
+
 from setuptools import find_packages, setup
+
+with open("src/leet2git/version.py") as file:
+    version_match = re.search(r'__version__ = "(?P<version>.*)"', file.read())
+    if version_match is None:
+        raise ValueError("The version is not specified in the version.py file.")
+    version = version_match["version"]
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
 
 setup(
     name="leet2git",
-    version="0.1.0",
+    version=version,
+    description="Tool to ease the integration between Leetcode and a Git repository",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yurirocha15/leetcode2github",
+    author="Yuri Rocha",
+    author_email="yurirocha15@gmail.com",
+    license="MIT License",
     packages=find_packages("src"),
     package_dir={"": "src"},
     package_data={"leet2git": ["py.typed"]},
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob("src/leet2git/*.py")],
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
+    ],
     install_requires=[
         "appdirs == 1.4.4",
         "autoimport == 0.7.0",
