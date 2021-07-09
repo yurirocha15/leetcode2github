8786e1f5cabed0119abfdeb9e8c25ba6d1c4643f
Yuri Rocha <yurirocha15@gmail.com>
2021-07-09 20:58:22 +0900

    [TEST] test version bumper (#31)
    * add version bumper
    
    * update app version
    
    * use on test branch first

--- src/leet2git/version.py
+++ src/leet2git/version.py
@@ -1,4 +1,6 @@
+import os
 import platform
+import re
 import sys
 
 __version__ = "0.0.3"
@@ -12,3 +14,27 @@ def version_info() -> str:
         "platform": platform.platform(),
     }
     return "\n".join(f"{k + ':' :>30} {v}" for k, v in info.items())
+
+
+def update_version_string(new_version: str):
+    if not re.match(r"^(\d+\.)?(\d+\.)?(\d+)$", new_version):
+        print(f"Version {new_version} is not valid")
+        return
+
+    file_path = os.path.abspath(__file__)
+    version_regex = re.compile(r"(^_*?version_*?\s*=\s*['\"])(\d+\.\d+\.\d+)", re.M)
+    with open(file_path, "r+") as f:
+        content = f.read()
+        f.seek(0)
+        f.write(
+            re.sub(
+                version_regex,
+                lambda match: "{}{}".format(match.group(1), new_version),
+                content,
+            )
+        )
+        f.truncate()
+
+
+if __name__ == "__main__":
+    update_version_string(sys.argv[1])
