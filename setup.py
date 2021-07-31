b91f83abbe56e09d2357025bd6621b33e548ba2c
Yuri Rocha <yurirocha15@gmail.com>
2021-08-01 01:31:46 +0900

    update requirements

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import io
 import os
 import re
 from glob import glob
@@ -12,6 +13,21 @@ with open("src/leet2git/version.py") as file:
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
+
+
+def pip(filename):
+    """Parse pip reqs file and transform it to setuptools requirements."""
+    requirements = []
+    for line in io.open(os.path.join("requirements", f"{filename}.txt")):
+        line = line.strip()
+        if not line or "://" in line or line.startswith("#"):
+            continue
+        requirements.append(line)
+    return requirements
+
+
+install_require = pip("requirements")
+dev_require = pip("requirements-dev")
 
 setup(
     name="leet2git",
@@ -44,37 +60,10 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Topic :: Utilities",
     ],
-    install_requires=[
-        "appdirs == 1.4.4",
-        "autoimport == 0.7.0",
-        "beautifulsoup4 == 4.9.3",
-        "browser-cookie3 == 0.12.1",
-        "click == 8.0.1",
-        "black == 21.5b2",
-        "isort == 5.6.4",
-        "GitPython == 3.1.18",
-        "requests == 2.25.1",
-    ],
-    extra_require={
-        "dev": [
-            "flake8 == 3.8.4",
-            "flake8-bugbear == 20.11.1",
-            "flake8-builtins == 1.5.3",
-            "flake8-polyfill == 1.0.2",
-            "flake8-pytest == 1.3",
-            "flake8-use-fstring == 1.1",
-            "mypy == 0.790",
-            "mypy-extensions == 0.4.3",
-            "pep8-naming == 0.11.1",
-            "pre-commit == 2.9.2",
-            "pycodestyle == 2.6.0",
-            "pyflakes == 2.2.0",
-            "pytest == 6.1.2",
-            "pytest-cov == 2.10.1",
-            "pytest-custom-exit-code == 0.3.0",
-            "pytest-flake8 == 1.0.6",
-            "pytest-mypy == 0.8.0",
-        ]
+    install_requires=install_require,
+    dev_require=dev_require,
+    extras_require={
+        "dev": dev_require,
     },
     entry_points={
         "console_scripts": [
